fb7d21c91e35530a66b5430dd659d7700ac9cb3c
GP <dipeshkataria3@gmail.com>
2020-04-12 15:56:48 +0530

    Some Activity removed default

--- app/src/main/java/com/androdocs/textrecognizer/Meaning.java
+++ app/src/main/java/com/androdocs/textrecognizer/Meaning.java
@@ -2,6 +2,7 @@ package com.androdocs.textrecognizer;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
+import android.text.TextPaint;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 
 public class Meaning extends AppCompatActivity {
@@ -29,28 +31,60 @@ public class Meaning extends AppCompatActivity {
 
         lvOG=(ListView) findViewById(R.id.listViewOriginal);
         lvOG.setAdapter(new CustomAdapter(this, list));
-/*        SpannableString ss = new SpannableString(value);
-        for(final String word : list){
-            if(word.startsWith(".")){
-                ClickableSpan clickableSpan = new ClickableSpan() {
-                    @Override
-                    public void onClick(View textView) {
-                        //use word here to make a decision
-                        Spanned sp = (Spanned) ((TextView)textView).getText();
-                        start = sp.getSpanStart(this);
-                        end = sp.getSpanEnd(this);
-                        String word = sp.subSequence(start, end).toString();
-                        Toast.makeText(getApplicationContext(),word,Toast.LENGTH_SHORT).show();
-                    }
-                };
-                ss.setSpan(clickableSpan,start, end + word.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            }
-        }
-        TextView textView = (TextView) findViewById(R.id.textSpan);
-        textView.setText(ss);
-        textView.setMovementMethod(LinkMovementMethod.getInstance());*/
 
+
+
+
+        TextView textView = (TextView) findViewById(R.id.textSpan);
+        String completeString = value;
+        final int arraySize = list.length;
+
+        for(int i = 0; i < arraySize; i++)
+        {
+            final String singleElement = list[i];
+            String partToClick = singleElement;
+            createLink(textView, completeString, partToClick,
+                    new ClickableSpan() {
+                        @Override
+                        public void onClick(View widget) {
+                            // your action
+                            Toast.makeText(Meaning.this, "Start Sign up activity "+arraySize+singleElement, Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void updateDrawState(TextPaint ds) {
+                            super.updateDrawState(ds);
+                            // this is where you set link color, underline, typeface etc.
+                            int linkColor = ContextCompat.getColor(Meaning.this, R.color.colorAccent);
+                            ds.setColor(linkColor);
+                            ds.setUnderlineText(false);
+                        }
+                    }
+            );
+        }
     }
+    public static TextView createLink(TextView targetTextView, String completeString,
+                                      String partToClick, ClickableSpan clickableAction) {
+
+
+
+        SpannableString spannableString = new SpannableString(completeString);
+
+        // make sure the String is exist, if it doesn't exist
+        // it will throw IndexOutOfBoundException
+
+        int startPosition = completeString.indexOf(partToClick);
+        int endPosition = completeString.lastIndexOf(partToClick) + partToClick.length();
+
+        spannableString.setSpan(clickableAction, startPosition, endPosition,
+                Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+
+        targetTextView.setText(spannableString);
+        targetTextView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        return targetTextView;
+    }
+
 
 
 }
